0494d9bb61b88279b07b42d17f91de85337b7fff
Srikant <kumar_srikant@outlook.com>
2024-07-29 07:33:50 +0530

    Trying Vercel Again

--- main.py
+++ main.py
@@ -35,6 +35,6 @@ app.include_router(books_router, prefix="/books", tags=["Endpoints"])
 # app.include_router(summary_router)
 
 
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app)
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
